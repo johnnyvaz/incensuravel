1a8d3268b62d5b3bf2bc38bc868d92f72ba544dd
johnnyvaz <johnnyvaz@cod2d.com.br>
2024-08-25 18:46:37 -0300

    ok

--- components/InstructionsComponent.jsx
+++ components/InstructionsComponent.jsx
@@ -15,26 +15,39 @@ export default function InstructionsComponent() {
 
             <div className={styles.cards_container}>
                 <div className={styles.card}>
-                    <h2 className={styles.title}>Recursos do Perfil</h2>
+                    <h2 className={styles.title}>Propriedade Total</h2>
                     <p className={styles.text}>
-                        Identidade Digital Única e Descentralizada, Integração com Múltiplas Redes Sociais,
-                        Armazenamento Seguro e Imutável com Tecnologia Blockchain, Controle Total sobre a
-                        Privacidade e Acesso aos Dados.
+                        Ao armazenar seu perfil de forma descentralizada, você mantém total controle sobre seus dados. Isso significa que nenhuma empresa ou governo pode censurar, remover ou alterar seu perfil sem o seu consentimento.
                     </p>
                 </div>
                 <div className={styles.card}>
-                    <h2 className={styles.title}>Como Funciona</h2>
+                    <h2 className={styles.title}>Imutabilidade e Segurança</h2>
                     <p className={styles.text}>
-                        Registre-se e crie sua identidade digital utilizando sua carteira cripto. Conecte e
-                        sincronize suas contas de redes sociais ao seu perfil descentralizado. Aproveite a
-                        liberdade e segurança sem precedentes, com controle total sobre seus dados.
+                        A descentralização garante que seu perfil seja imutável e seguro. Os dados são armazenados em uma rede distribuída, reduzindo o risco de hackeamentos e garantindo que suas informações permaneçam intactas e acessíveis.
                     </p>
                 </div>
                 <div className={styles.card}>
-                    <h2 className={styles.title}>Comece Agora</h2>
+                    <h2 className={styles.title}>Integração de Redes Sociais</h2>
                     <p className={styles.text}>
-                        Pronto para assumir o controle da sua identidade online? Crie seu perfil descentralizado
-                        hoje mesmo e defina um novo padrão de liberdade na internet.
+                        Você poderá consolidar suas contas de redes sociais em um único perfil descentralizado. Isso simplifica o gerenciamento e a acessibilidade, permitindo que você tenha um ponto centralizado para todas as suas interações online.
+                    </p>
+                </div>
+                <div className={styles.card}>
+                    <h2 className={styles.title}>Privacidade e Controle</h2>
+                    <p className={styles.text}>
+                        Você pode definir quem tem acesso ao seu conteúdo e gerenciar suas permissões de forma mais granular. Isso melhora a privacidade e oferece uma maior liberdade de expressão, sem a preocupação com as políticas de censura das plataformas tradicionais.
+                    </p>
+                </div>
+                <div className={styles.card}>
+                    <h2 className={styles.title}>Sincronização Automática</h2>
+                    <p className={styles.text}>
+                        Seu conteúdo será sincronizado automaticamente com suas contas de redes sociais favoritas, garantindo que suas atualizações sejam refletidas em tempo real e sem esforço adicional da sua parte.
+                    </p>
+                </div>
+                <div className={styles.card}>
+                    <h2 className={styles.title}>Resiliência Contra Censura</h2>
+                    <p className={styles.text}>
+                        O modelo descentralizado é resistente à censura, proporcionando um ambiente onde você pode expressar suas opiniões e compartilhar informações sem medo de restrições impostas por terceiros.
                     </p>
                 </div>
             </div>
